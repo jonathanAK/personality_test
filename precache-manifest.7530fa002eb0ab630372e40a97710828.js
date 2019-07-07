5d3687a6684a034c7b5f417675878044aa1e1a61
jonathanak <jonathan@amitkohn.com>
2019-07-07 20:01:46 +0300

    Updates

---
+++ precache-manifest.7530fa002eb0ab630372e40a97710828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610b4d651791fb43f482de6c65da1f38",
+    "url": "/personality_test/index.html"
+  },
+  {
+    "revision": "9edd142fe52959950b05",
+    "url": "/personality_test/static/css/main.b010cc24.chunk.css"
+  },
+  {
+    "revision": "1477794088f943f2b759",
+    "url": "/personality_test/static/js/2.8a6ee972.chunk.js"
+  },
+  {
+    "revision": "9edd142fe52959950b05",
+    "url": "/personality_test/static/js/main.d1e287be.chunk.js"
+  },
+  {
+    "revision": "51b1d0a2892fa61b0728",
+    "url": "/personality_test/static/js/runtime~main.0d26c8fd.js"
+  },
+  {
+    "revision": "d3e32553ba59c3107715c1f4b44c22a9",
+    "url": "/personality_test/static/media/logo.d3e32553.png"
+  }
+]);
